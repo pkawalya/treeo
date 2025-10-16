20e62cab65640e1a38f333f70627d52c5f581734
Paul Kawalya <appcellon@gmail.com>
2025-10-16 17:16:14 +0300

    added BCC

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,7 @@ class AdminPanelProvider extends PanelProvider
                 'success' => '#15803d',
                 'warning' => '#a16207',
             ])
-            ->brandName('TreeO Admin')
+            ->brandName('BCC Admin')
             ->maxContentWidth(MaxWidth::Full)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
